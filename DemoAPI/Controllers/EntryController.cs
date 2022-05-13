beea02785b1a75054b07e56ff619f2bcb2011b32
Michael Stewart <mstewart@tangerineglobal.com>
2022-05-13 09:47:44 -0700

    Add project files.

---
+++ DemoAPI/Controllers/EntryController.cs
@@ -0,0 +1,62 @@
+﻿using DemoLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EntryController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public EntryController(ILogger<EntryController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<EntryController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EntryController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EntryController>
+        [HttpPost]
+        [Route("Person")]
+        public void Post([FromBody] PersonModel value)
+        {
+            _logger.LogInformation(value.PersonInfo);
+        }
+
+        // POST api/<EntryController>
+        [HttpPost]
+        [Route("Address")]
+        public void Post([FromBody] AddressModel address)
+        {
+            _logger.LogInformation(address.FullAddress);
+        }
+
+        // PUT api/<EntryController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EntryController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DemoAPI/Controllers/GreetingsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        private ILogger _logger;
+
+        public GreetingsController(ILogger<GreetingsController> logger)
+        {
+            _logger = logger;
+        }
+        //// GET: api/<GreetingsController>
+        //[HttpGet]
+        //public IEnumerable<string> Get(string firstName = "", string lastName = "")
+        //{
+        //    List<string> output = new List<string>();
+        //    if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
+        //    {
+        //        output.Add($"Hello {firstName} {lastName}");
+        //    }
+        //    else
+        //    {
+        //        output.Add("Hello world!");
+        //    }
+        //    return output;
+        //}
+
+        // GET api/<GreetingsController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GreetingsController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            _logger.LogInformation(value);
+        }
+
+        // PUT api/<GreetingsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GreetingsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
