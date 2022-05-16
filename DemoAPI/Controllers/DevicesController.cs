3ce1994c20fe223263639410895126d25f0130ba
Michael Stewart <mstewart@tangerineglobal.com>
2022-05-16 12:31:15 -0700

    Demo methods in DevicesController

---
+++ DemoAPI/Controllers/DevicesController.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using Hospitality.Data;
+using Hospitality.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi.Controllers
+{
+    public class DevicesController : Controller
+    {
+        static HospitalityContext context;
+
+        public DevicesController(HospitalityContext hospitalityContext)
+        {
+            context = hospitalityContext;
+        }
+
+        [HttpGet("Search/Devices")]
+        [ProducesResponseType(typeof(List<Device>), (int)HttpStatusCode.OK)]
+        public IActionResult GetAllDevices()
+        {
+            return Ok(context.Devices.Where(d => d.OperatorID == 100018 && d.AccountDevice != null).ToList());
+        }
+
+        [HttpGet("Search/Devices/{macAddress}")]
+        [ProducesResponseType(typeof(Device), (int)HttpStatusCode.OK)]
+        public IActionResult GetDevice(string macAddress)
+        {
+            var device = context.Devices.Where(d => d.OperatorID == 100018 && d.MACAddress == macAddress).FirstOrDefault();
+            if (device!= null)
+                return Ok(device);
+            else
+                return BadRequest();
+        }
+
+        [HttpPost("NewDevice/{macAddress}")]
+        public IActionResult PostNewDevice(string macAddress)
+        {
+            var device = new Device
+            {
+                ID = Guid.NewGuid(),
+                MACAddress = macAddress,
+                OperatorID = 100018,
+                ModelID = Guid.Parse("9817f7c9-9ef6-4775-9be8-074d713358a5"),
+                StatusID = 1,
+                SerialNumber = "12345"
+            };
+            context.Devices.Add(device);
+            return Created("Search/Devices/{macAddress}", context.SaveChanges());
+        }
+
+        [HttpPut("Update/{macAddress}/{statusID}")]
+        public IActionResult UpdateStatusCode(string macAddress, int statusID)
+        {
+            var device = context.Devices.Where(d => d.OperatorID == 100018 && d.MACAddress == macAddress).FirstOrDefault();
+            if (device == null)
+                return BadRequest();
+            device.StatusID = statusID;
+            return Ok(context.SaveChanges());
+        }
+
+        [HttpDelete("Delete/{macAddress}")]
+        public IActionResult DeleteDevice(string macAddress)
+        {
+            var device = context.Devices.Where(d => d.OperatorID == 100018 && d.MACAddress == macAddress).FirstOrDefault();
+            context.Remove(device);
+            return Ok(context.SaveChanges());
+        }
+    }
+}
--- DemoAPI/Program.cs
+++ DemoAPI/Program.cs
@@ -1,25 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using Hospitality.Data;
+using System.Text.Json.Serialization;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace DemoApi
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static IConfiguration Configuration =>
+            new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+               .Build();
+
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDbContext<HospitalityContext>(options => options.UseSqlServer(Configuration.GetConnectionString("HospitalityDb")));
+            builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
